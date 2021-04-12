b34f4864abdc3ca0249e108f0cb885ec77ddee42
Dino <dinocraftdidi@gmail.com>
2021-04-12 11:31:38 +0200

    Init

---
+++ app/src/main/java/com/example/webbrowser/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.webbrowser;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View v) {
+                String search = ((TextInputEditText) findViewById(R.id.textInputEditText)).getText().toString();
+                String extention = ((Spinner) findViewById(R.id.spinner)).getSelectedItem().toString();
+                WebView webView = (WebView) findViewById(R.id.webView);
+
+                webView.loadUrl("http://" + shuffle(search) + extention);
+            }
+        });
+    }
+
+    public String shuffle(String input){
+        List<Character> characters = new ArrayList<Character>();
+        for(char c:input.toCharArray()){
+            characters.add(c);
+        }
+        StringBuilder output = new StringBuilder(input.length());
+        while(characters.size()!=0){
+            int randPicker = (int)(Math.random()*characters.size());
+            output.append(characters.remove(randPicker));
+        }
+
+        return output.toString();
+    }
+}
